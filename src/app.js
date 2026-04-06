39c3b70177360efa49668cc9873c8c970b37ab65
joao <dev@monitoo.com.br>
2026-04-06 11:20:18 -0300

    feat: adicionando middleware de rota nao encontrada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import clientesRoute from "./routes/clientesRoutes.js"
 import { logger } from "./middlewares/loggerMiddleware.js"
+import { notFound } from "./middlewares/notFoundMiddleware.js";
 
 
 const app = express();
@@ -12,6 +13,9 @@ app.use(logger);
 
 // rotas 
 app.use(clientesRoute);
+
+// middleware de rota não encontrada
+app.use(notFound);
 
 
 export default app;
